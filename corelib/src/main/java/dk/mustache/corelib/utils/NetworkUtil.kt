f8f541a2ee19f3f43e88431776020e08b53f7a4b
Mikkel Jørgensen <mj@mustache.dk>
2019-04-16 11:29:44 +0200

    added NetworkUtil

---
+++ corelib/src/main/java/dk/mustache/corelib/utils/NetworkUtil.kt
@@ -0,0 +1,31 @@
+package dk.mustache.corelib.utils
+
+import android.net.NetworkInfo
+import android.Manifest.permission
+import android.content.Context
+import androidx.annotation.RequiresPermission
+import android.net.ConnectivityManager
+
+/**
+ * Check if there is any connectivity
+ *
+ * @param context
+ *
+ * @return [true] if connected [false] otherwise
+ */
+@RequiresPermission(permission.ACCESS_NETWORK_STATE)
+fun isConnected(context: Context): Boolean {
+    return getNetworkInfo(context).isConnected
+}
+
+/**
+ * Get the network info
+ *
+ * @param context
+ *
+ * @return Network info
+ */
+@RequiresPermission(permission.ACCESS_NETWORK_STATE)
+private fun getNetworkInfo(context: Context): NetworkInfo {
+    return (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
+}
