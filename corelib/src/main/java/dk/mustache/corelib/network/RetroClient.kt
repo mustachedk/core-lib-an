b1fb09f2decbff03560b0084d70ee37e3256666a
MikkelSJ <msj@mustache.dk>
2020-09-15 10:41:09 +0200

    added comment

--- corelib/src/main/java/dk/mustache/corelib/network/RetroClient.kt
+++ corelib/src/main/java/dk/mustache/corelib/network/RetroClient.kt
@@ -4,7 +4,11 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-/** Singleton retroFitClient with GSON parser and OkHttpInterceptor which automatically retries services failed with missing auth/401 **/
+/** Singleton retroFitClient with
+ * - GSON parser
+ * - OkHttpTokenRefreshAuthenticator which signs request with a new access token if needed
+ * - OkHttpAuthorizationInterceptor which automatically retries services failed with missing auth/401
+ * **/
 
 object RetroClient {
     fun getRetrofitInstance(
