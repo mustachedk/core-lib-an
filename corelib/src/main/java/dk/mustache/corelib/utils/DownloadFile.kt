9309aa321576db4617158be5a27e177f4e611aae
Andreas <andreasmr@gmail.com>
2020-11-25 07:28:05 +0100

    Added DownloadFile util

---
+++ corelib/src/main/java/dk/mustache/corelib/utils/DownloadFile.kt
@@ -0,0 +1,54 @@
+package dk.mustache.corelib.utils
+
+import android.annotation.SuppressLint
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.observers.DisposableObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.io.BufferedReader
+import java.net.URL
+
+object DownloadFile {
+
+    @SuppressLint("CheckResult")
+    fun withCallback(link: String, callback: (read: String) -> Unit) {
+        var json = ""
+        val observable = Observable.fromCallable {
+            return@fromCallable try {
+                val content = StringBuilder()
+                URL(link).openStream().use { input ->
+                    val reader = BufferedReader(input.reader())
+                    try {
+                        var line = reader.readLine()
+                        while (line != null) {
+                            content.append(line)
+                            line = reader.readLine()
+                        }
+                    } finally {
+                        reader.close()
+                    }
+                }
+                content.toString()
+
+            } catch (e: Exception) {
+                ""
+            }
+        }
+        observable.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(@SuppressLint("CheckResult")
+            object: DisposableObserver<String>() {
+                override fun onNext(hasInternet: String) {
+                    callback(hasInternet)
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+                override fun onComplete() {
+
+                }
+            })
+    }
+}
