1964de72566bcbac0c6dd360f292687aff23ffb7
Andreas <andreasmr@gmail.com>
2021-04-19 13:51:59 +0200

    check bluetooth status

---
+++ corelib/src/main/java/dk/mustache/corelib/bluetooth/BluetoothManager.kt
@@ -0,0 +1,26 @@
+package dk.mustache.corelib.bluetooth
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.processors.BehaviorProcessor
+
+object BluetoothHelper {
+    const val REQUEST_ENABLE_BT = 0
+    private val bluetoothAdapter = lazy { BluetoothAdapter.getDefaultAdapter() }
+
+    fun isBluetoothEnabled(): Boolean {
+        return bluetoothAdapter.value!=null && bluetoothAdapter.value.isEnabled
+    }
+
+    fun enableBluetooth(activity: Activity) {
+        if (!bluetoothAdapter.value.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+    }
+}
