3495af1b372dbdc169ea6b9372a9969e82194483
Andreas <andreasmr@gmail.com>
2021-10-14 14:22:08 +0200

    Updated OrientationAwareRecyclerView and version bump

--- corelib/src/main/java/dk/mustache/corelib/orientationaware_recyclerview/OrientationAwareRecyclerView.kt
+++ corelib/src/main/java/dk/mustache/corelib/orientationaware_recyclerview/OrientationAwareRecyclerView.kt
@@ -6,7 +6,7 @@ import android.view.MotionEvent
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.abs
 
-class OrientationAwareRecyclerView @JvmOverloads constructor(
+open class OrientationAwareRecyclerView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : RecyclerView(context, attrs, defStyleAttr) {
