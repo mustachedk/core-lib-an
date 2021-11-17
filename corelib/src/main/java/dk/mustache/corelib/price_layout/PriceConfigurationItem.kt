9f8de2a9272b5cee0ba1d76120fa33007283aa6c
Andreas <andreasmr@gmail.com>
2021-11-17 23:21:33 +0100

    Update PriceConfigurationItem.kt

--- corelib/src/main/java/dk/mustache/corelib/price_layout/PriceConfigurationItem.kt
+++ corelib/src/main/java/dk/mustache/corelib/price_layout/PriceConfigurationItem.kt
@@ -1,11 +1,16 @@
 package dk.mustache.corelib.price_layout
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.io.Serializable
+
+@Parcelize
 data class PriceConfigurationItem(
     val discountPrice: Double,
     val price: Double,
     val discountAmount: Double,
     val discountMaxQuantity: Double
-) {
+):Parcelable, Serializable {
 
     fun displayYellowOfferPrice(): Boolean {
         return discountAmount == 1.0
