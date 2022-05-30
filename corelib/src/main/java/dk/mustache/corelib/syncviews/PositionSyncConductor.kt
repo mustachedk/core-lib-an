7ec5337b89fc022d30e5cab68daae01c74919c6b
tsteen <ts@mustache.dk>
2022-05-30 15:14:13 +0200

    Remove SyncView from commit

--- corelib/src/main/java/dk/mustache/corelib/syncviews/PositionSyncConductor.kt
+++
@@ -1,9 +0,0 @@
-package dk.mustache.corelib.syncviews
-
-class PositionSyncConductor: SyncConductor<() -> Int>() {
-
-
-    override fun transmitToOthers(transmitter: SyncSubject<() -> Int>, msg: () -> Int) {
-        super.transmitToOthers(transmitter, msg)
-    }
-}
