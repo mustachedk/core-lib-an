8b785b3878d4513a0e19a23accf17df189e1fc42
tsteen <ts@mustache.dk>
2023-01-19 09:39:28 +0100

    Test fixes & version bump

--- corelib/src/test/java/dk/mustache/corelib/mdate/MDateFormatTimeZoneTest.kt
+++ corelib/src/test/java/dk/mustache/corelib/mdate/MDateFormatTimeZoneTest.kt
@@ -3,37 +3,26 @@ package dk.mustache.corelib.mdate
 import dk.mustache.corelib.utils.MDate
 import dk.mustache.corelib.utils.MDateBuilder
 import dk.mustache.corelib.utils.MDateFormat
+import org.junit.After
 import org.junit.Assert
-import org.junit.Rule
 import org.junit.Test
-import org.junitpioneer.jupiter.DefaultTimeZone
 import java.util.*
 
 class MDateFormatTimeZoneTest {
 
+    @After
+    fun doAfter() {
+        MDate.setDefaultTimeZone(TimeZone.getDefault())
+    }
+
     @Test
     fun specifyTimeZoneOnMDateFormat() {
         // Arrange
-        MDate.setDefaultTimeZone(TimeZone.getTimeZone("PRC")) // China Standard Time (+8)
+        MDate.setDefaultTimeZone(TimeZone.getTimeZone("PRC"))
 
         val date = MDateBuilder(timeZone = TimeZone.getTimeZone("UTC"))
             .dateTime(month = 1, hour = 12, minute = 55)
             .build() // Month to 1 to avoid daylight savings
-        val expectedOutput = "20:55"
-
-        // Act
-        val newDate = date.show(MDateFormat.TIME, localTimeZone = true) // From DefaultTimeZone, our local timezone is "PRC"
-
-        // Assert
-        Assert.assertEquals(expectedOutput, newDate)
-    }
-
-    @Test
-    @DefaultTimeZone("PRC") // This sets TimeZone.getDefault() during this test
-    fun specifyTimeZoneOnMDateFormatDefault() {
-        // Arrange
-        val date = MDateBuilder(timeZone = TimeZone.getTimeZone("UTC"))
-            .dateTime(month = 1, hour = 12, minute = 55).build() // Month to 1 to avoid daylight savings
         val expectedOutput = "20:55"
 
         // Act
